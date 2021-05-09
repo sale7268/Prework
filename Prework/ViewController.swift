c2187945d816d7c47edfe949451bf65495c673a5
Hamza Saleem <hsaleem@csumb.edu>
2021-05-09 07:20:22 -0600

    Updated with additional stories

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -8,25 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var numberOnScreen:Double = 0;
 
-    @IBOutlet weak var billAmountTextField: UITextField!
-    
+    @IBOutlet weak var billAmountLabel: UILabel!
     @IBOutlet weak var tipAmountLabel: UILabel!
-    
     @IBOutlet weak var tipControl: UISegmentedControl!
-    
     @IBOutlet weak var totalLabel: UILabel!
+    @IBAction func numbers(_ sender: UIButton) {
+        if(billAmountLabel.text != String(0)){
+            billAmountLabel.text = billAmountLabel.text! + String(sender.tag-1)
+            numberOnScreen = Double(billAmountLabel.text!)!
+        } else {
+            billAmountLabel.text = String(sender.tag-1)
+            numberOnScreen = Double(billAmountLabel.text!)!
+        }
+    }
+    
+    @IBAction func eraseBtn(_ sender: UIButton) {
+        billAmountLabel.text = String(0)
+    }
+    
+    @IBAction func dotBtn(_ sender: UIButton) {
+        billAmountLabel.text = billAmountLabel.text! + String(".")
+        numberOnScreen = Double(billAmountLabel.text!)!
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
     }
 
 
     @IBAction func calculateTip(_ sender: Any) {
         
-        let bill = Double(billAmountTextField.text!) ?? 0
+        let bill = Double(billAmountLabel.text!) ?? 0
         
         //Get total by multiplying
         let tipPercentages = [0.15,0.18,0.2]
